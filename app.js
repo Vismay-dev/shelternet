ad2b190251e9d7a0a9681a2dae449be947d69e93
Vismay-dev <vismaysuramwar@gmail.com>
2022-02-08 21:14:46 +0400

    Full

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const express = require('express')
 
 // app.use(methodOverride('_method'))
 // app.use(secure)
-app.use(mongoSanitize())
+// app.use(mongoSanitize())
 dotenv.config()
 
 cloudinary.config({
